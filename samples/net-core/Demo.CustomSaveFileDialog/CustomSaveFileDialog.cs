928a6048f9078843ee9778182ee7e4e8a86ef3cf
Etienne Charland <mysteryx93@protonmail.com>
2021-12-09 10:41:08 -0500

    Fix demo

--- samples/net-core/Demo.CustomSaveFileDialog/CustomSaveFileDialog.cs
+++ samples/net-core/Demo.CustomSaveFileDialog/CustomSaveFileDialog.cs
@@ -1,18 +1,21 @@
-﻿using MvvmDialogs.FrameworkDialogs;
+﻿using System.IO;
+using System.Threading.Tasks;
+using MvvmDialogs.FrameworkDialogs;
 using MvvmDialogs.Wpf;
 using MvvmDialogs.Wpf.FrameworkDialogs;
 using Ookii.Dialogs.Wpf;
 
 namespace Demo.CustomSaveFileDialog
 {
-    public class CustomSaveFileDialog : FrameworkDialogBase<SaveFileDialogSettings>
+    public class CustomSaveFileDialog : FrameworkDialogBase<SaveFileDialogSettings, string>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="CustomSaveFileDialog"/> class.
         /// </summary>
         /// <param name="settings">The settings for the save file dialog.</param>
-        public CustomSaveFileDialog(SaveFileDialogSettings settings)
-            : base(settings)
+        /// <param name="appSettings">Application-wide settings configured on the DialogService.</param>
+        public CustomSaveFileDialog(SaveFileDialogSettings settings, AppDialogSettings appSettings)
+            : base(settings, appSettings)
         {
         }
 
@@ -25,31 +28,26 @@ namespace Demo.CustomSaveFileDialog
         /// <returns>
         /// true if user clicks the OK button; otherwise false.
         /// </returns>
-        public override bool? ShowDialogAsync(WindowWrapper owner)
+        public override Task<string> ShowDialogAsync(WindowWrapper owner)
         {
+            var s = Settings;
+            var fileInfo = !string.IsNullOrEmpty(s.InitialPath) ? new FileInfo(s.InitialPath) : null;
             var saveFileDialog = new VistaSaveFileDialog
             {
-                AddExtension = Settings.AddExtension,
-                CheckFileExists = Settings.CheckFileExists,
-                CheckPathExists = Settings.CheckPathExists,
-                CreatePrompt = Settings.CreatePrompt,
-                DefaultExt = Settings.DefaultExtension,
-                FileName = Settings.FileName,
-                Filter = Settings.Filter,
-                FilterIndex = Settings.FilterIndex,
-                InitialDirectory = Settings.InitialDirectory,
-                OverwritePrompt = Settings.OverwritePrompt,
-                Title = Settings.Title
+                AddExtension = !string.IsNullOrEmpty(s.DefaultExtension),
+                CheckFileExists = s.CheckFileExists,
+                CheckPathExists = s.CheckPathExists,
+                CreatePrompt = s.CreatePrompt,
+                DefaultExt = s.DefaultExtension,
+                FileName = fileInfo?.Name,
+                InitialDirectory = fileInfo?.DirectoryName,
+                OverwritePrompt = s.OverwritePrompt,
+                Title = s.Title
+                // Filter = s.Filter,
             };
 
             var result = saveFileDialog.ShowDialog(owner.Ref);
-
-            // Update settings
-            Settings.FileName = saveFileDialog.FileName;
-            Settings.FileNames = saveFileDialog.FileNames;
-            Settings.FilterIndex = saveFileDialog.FilterIndex;
-
-            return result;
+            return Task.FromResult(result == true ? saveFileDialog.FileName : null);
         }
     }
 }
